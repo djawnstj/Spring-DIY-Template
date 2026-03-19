ac03e360391f62e01671d67a02c09681a558df38
dongheeko <qmdlfakrhf@naver.com>
2026-03-20 07:26:16 +0900

    chore: 임베디드 톰캣 실행 및 jsp 서빙 확인

--- src/main/java/com/diy/app/Main.java
+++ src/main/java/com/diy/app/Main.java
@@ -1,7 +1,9 @@
 package com.diy.app;
 
+import com.diy.framework.web.server.TomcatWebServer;
+
 public class Main {
     public static void main(String[] args) {
-
+        new TomcatWebServer().start();
     }
 }
